d3cd01b3ee14ec5f3eb3e84fe13b0b3bce09e5e8
DanyiKaroly <dany.karoly815@gmail.com>
2024-05-06 16:27:26 +0200

    vég1

--- WebTechRDC1L4/server.js
+++ WebTechRDC1L4/server.js
@@ -10,6 +10,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main', 'index.html'));
 });
 
+// 404 hibakezelés
+app.use((req, res, next) => {
+  res.status(404).send("A keresett oldal nem található.");
+});
+
+// Általános hibakezelés
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Valami hiba történt a szerveren.');
+});
+
 app.listen(port, () => {
     console.log(`A szerver fut a http://localhost:${port} címen.`);
 });
